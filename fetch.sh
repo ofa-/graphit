cf9189375fc12c43a1e76c5a09d20028d9ee2c5b
ofa <ofa@github>
2020-10-14 22:00:49 +0200

    add fetch for departments

--- fetch.sh
+++ fetch.sh
@@ -8,3 +8,8 @@ DATA_SET=6fadff46-9efd-4c53-942a-54aca783c30c
 curl -sL $DATA_URL/$DATA_SET > data.csv
 
 sed -i '/91;2020-09-18/ d' data.csv
+
+
+[ -f dep.csv ] || curl -s \
+https://www.regions-et-departements.fr/fichiers/departements-francais.csv \
+| sed '101,$ d' | sed '2,10 s/^/0/' > dep.csv
