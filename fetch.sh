61743eb1d3863d53870d66e827819641c03b054e
ofa <ofa@github>
2020-11-09 23:30:04 +0100

    fix data cleanup stage, add dept 59 & 95 cleanup

--- fetch.sh
+++ fetch.sh
@@ -7,7 +7,12 @@ DATA_SET=6fadff46-9efd-4c53-942a-54aca783c30c
 
 curl -sL $DATA_URL/$DATA_SET > data.csv
 
-sed -i '/91;2020-09-18/ d' data.csv
+sed -i '
+	/91;2020-09-18/ s_;76;_;;_
+	/59;2020-07-07/ s_;7;_;;_
+	/59;2020-07-08/ s_;16;_;;_
+	/95;2020-09-16/ s_;19;_;;_
+' data.csv
 
 
 [ -f dep.csv ] || curl -s \
