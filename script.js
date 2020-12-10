8628c815a2c8181dfa6fe0ee9e4323686a7f39e2
ofa <ofa@github>
2020-12-10 22:35:02 +0100

    rework index/script.js

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
+var _region = {
+	"survey": "13,34,30,31,33,69,42,38,73,74,05,67,76,59,75,pc,gc,idf,met",
+	"paca": "04,05,06,13,83,84",
+	"rhone-alpes": "69,38,01,26,73,74",
+	"auvergne": "03,07,15,42,43,63",
+	"bretagne": "22,29,35,56",
+	"normandie": "14,27,50,61,76",
+	"aquitaine": "16,17,19,23,24,33,40,47,64,79,86,87",
+	"occitanie": "09,11,12,30,31,32,34,46,48,65,66,81,82",
+	"grand-est": "08,10,51,52,54,55,57,67,68,88",
+	"centre-loire": "18,28,36,37,41,45",
+	"pays-loire": "44,49,53,72,85",
+	"bourgogne": "21,58,71,89",
+	"franche-comte": "25,39,70,90",
+	"hauts-france": "02,59,60,62,80",
+	"ile-de-france": "75,77,78,91,92,93,94,95",
+	"sud": "34,30,13,83,06,2A,2B",
+}
+
+var base = "https://coviiid.github.io/fig/"
+
 
 function onload() {
-	document.body.innerHTML = document
-		.querySelector("noscript").textContent
 
-	var images = document.images
-	var base = "fig/"
+	var region = _region["survey"].split(",")
 
-	for (var i=0; i < images.length; i++) {
-		var img = images[i]
+	for (var i=0; i < region.length; i++) {
+		var img = document.createElement("img")
 		img.style.display = "none"
-		img.onload = create_areas
-		img.next = images[(i+1) % images.length]
-		img.prev = images[(i-1+images.length) % images.length]
-		img.src = base + img.src.replace(/.*\//, "")
+		img.setAttribute("usemap", "#fig")
+		img.src = base + region[i] + ".png"
+		document.body.appendChild(img)
 	}
-	images.curr = images[images.length-1]
-	images.curr.next = images.curr.prev
-	images.curr.prev.next = images[0]
-	images[0].prev = images.curr.next
-	images.home = images.curr
-	images.curr.src = images.curr.src.replace("/fig/", "/full/")
-	images.curr.setAttribute("class", "full")
-
+	set_loop(document.images)
+	add_img_areas()
+	add_home_img()
 	add_help()
-	show(images.curr)
-	var query = document.location.search.substring(1)
-	if (query) show_query(query)
 
 	document.onkeydown = onkeypress
+
+	var query = document.location.search.substring(1)
+	if (query)
+		show_query(query)
+	else
+		show_home()
 }
 
 function show_query(query) {
@@ -41,6 +56,32 @@ function show_query(query) {
 		show(img)
 		break
 	}
+}
+
+function set_loop(images) {
+	var img, len = images.length
+	for (var i=0; i < len; i++) {
+		img = images[i]
+		img.next = images[(i+1) % len]
+		img.prev = images[(i-1+len) % len]
+	}
+}
+
+function add_img_areas() {
+	document.images[0].onload = create_areas
+}
+
+function add_home_img() {
+	var home = document.createElement("img")
+	home.src = base + "../full/met-full.png"
+	home.setAttribute("class", "full")
+	home.onclick = show_next
+	home.next = document.images[0]
+	home.prev = home.next.prev
+
+	document.body.append(home)
+	document.images.home = home
+	document.images.curr = home
 }
 
 function add_help() {
@@ -72,7 +113,7 @@ function create_areas() {
 	var width = img.width
 	var height = img.height
 	var map = document.createElement("map")
-	map.name = img.src.replace(/.*\//, "")
+	map.name = "fig"
 	map.innerHTML = (
 	"<area shape='rect' coords='" +
 		"0,0," + width + ",69'>" +
@@ -84,7 +125,6 @@ function create_areas() {
 	map.children[0].onclick = show_home
 	map.children[1].onclick = show_prev
 	map.children[2].onclick = show_next
-	img.setAttribute("usemap", "#" + map.name)
 	document.body.appendChild(map)
 }
 
